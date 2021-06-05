6e183d0bc796afaaab53df14821ad6c8b2eaaecd
yihsiulee <s30401s@gmail.com>
2021-06-06 02:40:01 +0800

    no message

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import ccxt from 'ccxt'
 import { REACT_APP_USER1_APIKEY, REACT_APP_USER1_SECRET } from '../config'
 
-const ftx_exchange = new ccxt.ftx({
+const binance_exchange = new ccxt.binance({
   apiKey: REACT_APP_USER1_APIKEY,
   secret: REACT_APP_USER1_SECRET,
   timeout: 15000,
@@ -10,49 +10,49 @@ const ftx_exchange = new ccxt.ftx({
 
 // log出來的function可以直接call 如底下getPosition
 export const getAllImplicitApiMethods = () => {
-  return //console.log (ftx_exchange)
+  return //console.log (binance_exchange)
 }
 
 export const getExchageId = () => {
-  return ftx_exchange.id
+  return binance_exchange.id
 }
 
 export const getExchangeName = () => {
-  return ftx_exchange.name
+  return binance_exchange.name
 }
 
 export const getExchangeTime = () => {
-  return ftx_exchange.iso8601(ftx_exchange.milliseconds())
+  return binance_exchange.iso8601(binance_exchange.milliseconds())
 }
 
 export const getMarkets = () => {
-  return ftx_exchange.loadMarkets()
+  return binance_exchange.loadMarkets()
 }
 
 //獲取交易對數據
 export const getTicker = (symbol) => {
   if (!symbol) return
-  return ftx_exchange.fetchTicker(symbol)
+  return binance_exchange.fetchTicker(symbol)
 }
 
 //查詢餘額
 export const getBalance = () => {
-  return ftx_exchange.fetchBalance()
+  return binance_exchange.fetchBalance()
 }
 
 //查詢現在合約倉位資訊
 export const getPosition = () => {
-  return ftx_exchange.fetch_positions()
+  return binance_exchange.fetch_positions()
 }
 
 //取得帳戶資訊,裡面有很多資料
 export const getAccount = () => {
-  return ftx_exchange.private_get_account()
+  return binance_exchange.private_get_account()
 }
 
 //改變槓桿值
 export const changeLeverage = (userLeverage) => {
-  ftx_exchange.private_post_account_leverage({
+  binance_exchange.private_post_account_leverage({
     leverage: userLeverage,
   })
 }
@@ -61,5 +61,5 @@ export const changeLeverage = (userLeverage) => {
 //amount 開的數量
 //(保證金*槓桿 )/ 現在的幣價  = 最大可開的數量，最大可開數量 乘上 你要的開倉輸入的%數 就是開倉數量(amount)
 export const marketOrder = (symbol, side, amount) => {
-  ftx_exchange.createOrder(symbol, 'market', side, amount)
+  binance_exchange.createOrder(symbol, 'market', side, amount)
 }
