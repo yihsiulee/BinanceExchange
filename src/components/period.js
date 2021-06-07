d954ec5e30f7a4e2ad14accba1f2fba3d7231a69
taiyi <god830311@gmail.com>
2021-06-08 05:27:05 +0800

    把定時call api拉出去

---
+++ src/components/period.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useCallback, useContext } from 'react'
+import moment from 'moment'
+import { GlobalContext } from '../context'
+import { getServerTime } from '../api'
+
+const Period = () => {
+  const [global, setGlobal] = useContext(GlobalContext)
+
+  const callAPI = useCallback(async () => {
+    // 更新global
+    const timeData = await getServerTime()
+    setGlobal((prev) => {
+      return {
+        ...prev,
+        time: timeData['serverTime'], // 更新時間
+      }
+    })
+  }, [])
+
+  // 定時打API
+  const INTERVAL_TIME = 3000 // 間隔時間
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      callAPI()
+    }, INTERVAL_TIME)
+    return () => clearInterval(intervalId)
+  }, [callAPI])
+
+  return <></>
+}
+
+export default Period
